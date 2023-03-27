3ff7af5bdbbbba100f54f999422c58cf4cef673a
Aleksandr Krapivin <smoozmy@yandex.ru>
2023-03-27 11:30:39 +0300

    Поправлена кнопка лайка

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -60,7 +60,7 @@ extension ImagesListViewController {
         cell.dateLabel.text = dateFormatter.string(from: Date())
 
         let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "No Active")
+        let likeImage = isLiked ? UIImage(named: "Like Active") : UIImage(named: "Like No Active")
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
