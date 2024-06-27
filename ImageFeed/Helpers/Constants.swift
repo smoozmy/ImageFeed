16bb32462c9ebf98bf21f47f4ccc1bc94af17318
Aleksandr Krapivin <96174983+smoozmy@users.noreply.github.com>
2024-06-27 15:05:20 +0300

    add Constants

---
+++ ImageFeed/Helpers/Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+
+enum Constants {
+    static let accessKey = "ciLLVv3pPJzyByKPMFJXCuKcz0u2LRfe8zHao7byWx0"
+    static let secretKey = "QhcxMLNiG4d59PG1wLhXISOJlsFm_X3UJ3jx8PWLek4"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
+}
+
