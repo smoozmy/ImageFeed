f89c74ac206a1da6d635d50f9855feef9e025c8d
Aleksandr Krapivin <96174983+smoozmy@users.noreply.github.com>
2024-06-28 13:09:32 +0300

    add WebViewViewController

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    
+    // MARK: - UI and Lyfe Cycle
+    
+    private lazy var webView: WKWebView = {
+        let element = WKWebView()
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setView()
+        setupConstraints()
+    }
+    private func setView() {
+        view.addSubview(webView)
+    }
+    
+    // MARK: - Actions
+    
+}
+
+// MARK: - Constraints
+
+extension WebViewViewController {
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
