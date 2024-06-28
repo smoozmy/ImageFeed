2c9d809123f3d64bea372e5fcbd4116355399071
Aleksandr Krapivin <96174983+smoozmy@users.noreply.github.com>
2024-06-28 11:21:10 +0300

    AuthViewController - выполнена верстка экрана

--- ImageFeed/Login/AuthViewController.swift
+++ ImageFeed/Login/AuthViewController.swift
@@ -2,19 +2,47 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
+    
+    private lazy var logoUnsplash: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "LogoUnsplash")
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.backgroundColor = .ypWhite
+        element.setTitle("Войти", for: .normal)
+        element.setTitleColor(.ypBlack, for: .normal)
+        element.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        element.layer.cornerRadius = 16
+        element.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
     // MARK: - UI and Lyfe Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .ypBlack
         
         setView()
         setupConstraints()
     }
     private func setView() {
-        // Здесь добавляются новые элементы для отображения на экране
+        view.addSubview(logoUnsplash)
+        view.addSubview(loginButton)
     }
     
     // MARK: - Actions
+    
+    @objc private func didTapLoginButton() {
+        print("login")
+    }
     
 }
 
@@ -23,7 +51,16 @@ final class AuthViewController: UIViewController {
 extension AuthViewController {
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            // Здесь прописываются констрейнты для элементов
+            logoUnsplash.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoUnsplash.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logoUnsplash.widthAnchor.constraint(equalToConstant: 60),
+            logoUnsplash.heightAnchor.constraint(equalToConstant: 60),
+            
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            loginButton.heightAnchor.constraint(equalToConstant: 48),
+            loginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -90)
+            
         ])
     }
 }
