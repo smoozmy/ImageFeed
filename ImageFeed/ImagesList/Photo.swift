797c049d7f991d5fea784c8ff795837784c3a0d9
Aleksandr Krapivin <96174983+smoozmy@users.noreply.github.com>
2024-08-07 01:06:04 +0300

    Добавлены структуры для декодирования JSON

---
+++ ImageFeed/ImagesList/Photo.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct Photo {
+    let id: String
+    let size: CGSize
+    let createdAt: Date?
+    let welcomeDescription: String?
+    let thumbImageURL: String
+    let largeImageURL: String
+    let isLiked: Bool
+
+    init(from photoResult: PhotoResult) {
+        self.id = photoResult.id
+        self.size = CGSize(width: photoResult.width, height: photoResult.height)
+        self.createdAt = ISO8601DateFormatter().date(from: photoResult.createdAt ?? "")
+        self.welcomeDescription = photoResult.description
+        self.thumbImageURL = photoResult.urls.thumb
+        self.largeImageURL = photoResult.urls.full
+        self.isLiked = photoResult.likedByUser
+    }
+}
