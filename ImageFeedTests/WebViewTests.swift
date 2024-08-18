933c24e959b6da13ff050be35d36cc98cd0b6710
Aleksandr Krapivin <96174983+smoozmy@users.noreply.github.com>
2024-08-18 18:31:51 +0300

    add WebViewTests

---
+++ ImageFeedTests/WebViewTests.swift
@@ -0,0 +1,15 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Александр Крапивин on 18.08.2024.
+//
+
+@testable import ImageFeed
+import XCTest
+
+
+final class WebViewTests: XCTestCase {
+    
+    
+}
